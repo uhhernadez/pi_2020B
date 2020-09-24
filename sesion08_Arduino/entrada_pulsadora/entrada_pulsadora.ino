7f21ebcdddf5acf5b1f074d11af50d7ea1b67b26
Uriel Hernandez <uh.hernandez@ugto.mx>
2020-09-24 15:05:43 -0500

    Evitando el rebote

--- sesion08_Arduino/entrada_pulsadora/entrada_pulsadora.ino
+++ sesion08_Arduino/entrada_pulsadora/entrada_pulsadora.ino
@@ -4,19 +4,23 @@ const int ledPin = 11;
 int ledState = HIGH;
 int lastReading = LOW;
 
+int reboteEspera = 150;
+int ultimoCambio = 0;
+
 void setup() {
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
 }
 
-void loop() {
-  
+void loop() {  
   int reading = digitalRead(buttonPin);
 
-  if(reading == HIGH && lastReading == LOW) {
-    ledState = (ledState == HIGH)? LOW: HIGH;
+  if ((millis() - ultimoCambio) > reboteEspera) {
+    if(reading == HIGH && lastReading == LOW) {
+      ledState = (ledState == HIGH)? LOW: HIGH;
+      ultimoCambio = millis();
+    }
   }
-  
   digitalWrite(ledPin, ledState);
   lastReading = reading;
 }
