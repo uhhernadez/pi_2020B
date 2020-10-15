cd6ef8654465474601eab14a04e64ca667e41af6
Uriel Hernandez <uh.hernandez@ugto.mx>
2020-10-15 14:52:00 -0500

    Primera aproximación a la solución

--- sesion13/practica2/practica2.ino
+++ sesion13/practica2/practica2.ino
@@ -3,6 +3,7 @@
 // 3, 5, 6, 9, 10 PWD
 int led0 = 3, led1 =5, led2 = 6,  led3 = 9, led4 = 10;
 int seleccion = 0;
+int orientacion = true;
 
 void setup() {
   pinMode(led0, OUTPUT);
@@ -13,9 +14,9 @@ void setup() {
 }
 
 void loop() {
-  Secuencia(seleccion%5);
+  Secuencia(seleccion);
   delay(200);
-  seleccion++;
+  seleccion = Siguiente(seleccion);
 }
 
 void Secuencia(int led) {
@@ -32,4 +33,23 @@ void Secuencia(int led) {
     case 3:  digitalWrite(led3,HIGH); break;
     case 4:  digitalWrite(led4,HIGH); break;
   }
+}
+
+int Siguiente(int actual) {
+  // Cuando la orientacion es positiva se incrementa la variable
+  if(orientacion) {
+    if(actual == 4) {
+      orientacion = false;
+      return 3;  
+    }  
+    return actual + 1;
+  // Cuando la orientacion es negativa se decrementa
+  } else {
+    if(actual == 0) {
+      orientacion = true;
+      return 1;  
+    }    
+    return actual - 1;
+  }
+  
 }
