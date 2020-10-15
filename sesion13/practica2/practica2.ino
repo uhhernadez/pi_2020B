cf6a6c26fb6ae302ff35bf0d9a633c14d6c921bc
Uriel Hernandez <uh.hernandez@ugto.mx>
2020-10-15 15:40:20 -0500

    Programa final

--- sesion13/practica2/practica2.ino
+++ sesion13/practica2/practica2.ino
@@ -3,23 +3,59 @@
 // 3, 5, 6, 9, 10 PWD
 const int leds[] = {3, 5, 6, 9, 10};
 int pinIntensidad = A0;
+int pinVelocidad = 2;
+int pinAlto = 4;
 int seleccion = 0;
-int orientacion = true;
+int tiempoBoton = 100;
+int ultimoAlto = 0;
+int ultimoVelocidad = 0;
+
+int  velocidad = 1;
+bool orientacion = true;
+bool alto = false;
+
 
 void setup() {
     // 9600 Bauds
     Serial.begin(9600);
     pinMode(pinIntensidad, INPUT);
+    pinMode(pinVelocidad, INPUT);
+    pinMode(pinAlto,INPUT);
+    
     for (int k = 0; k < 5; k++) {
       pinMode(leds[k], OUTPUT);
     }
+    ultimoAlto = millis();
+    ultimoVelocidad = millis();
 }
 
 void loop() {
     int intensidad = analogRead(pinIntensidad);
-    Secuencia(seleccion, intensidad/4);
-    delay(200);
-    seleccion = Siguiente(seleccion);
+    int altoValor = digitalRead(pinAlto);
+    int velocidadValor = digitalRead(pinVelocidad);
+    
+    Serial.println(altoValor);
+    if(millis()- ultimoAlto > tiempoBoton ) {
+      if(altoValor == HIGH) {
+        alto = (alto == true)? false:true; 
+      }
+      ultimoAlto = millis();
+    }
+
+    if(millis()- ultimoVelocidad > tiempoBoton ) {
+      if(velocidadValor == HIGH) {
+        velocidad++;
+        velocidad = (velocidad%5)+1;
+        ultimoVelocidad = millis();
+      }
+    }
+    
+    if(!alto) {
+      Secuencia(seleccion, intensidad/4);
+      seleccion = Siguiente(seleccion);
+    }
+    
+    delay(velocidad * 50);
 }
 
 void Secuencia(int led, int intensidad) {
